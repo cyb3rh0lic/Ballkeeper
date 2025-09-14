aab44c3f828e013580d2254c224b9a5d2acc25ab
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-09-14 11:29:16 +0900

    chore: h2 데이터베이스 연관 삭제

--- src/main/java/capstone/ballkeeper/BallkeeperApplication.java
+++ src/main/java/capstone/ballkeeper/BallkeeperApplication.java
@@ -1,43 +1,12 @@
 package capstone.ballkeeper;
 
-import capstone.ballkeeper.domain.ReservationItem;
-import capstone.ballkeeper.domain.UsageStatus;
-import capstone.ballkeeper.domain.item.Item;
-import capstone.ballkeeper.domain.item.ItemStatus;
-import capstone.ballkeeper.domain.reservation.Reservation;
-import capstone.ballkeeper.domain.reservation.ReservationStatus;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class BallkeeperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BallkeeperApplication.class, args);
-
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("ballKeeper");
-		EntityManager em = emf.createEntityManager();
-
-		EntityTransaction tx = em.getTransaction();
-
-		tx.begin();
-
-		try {
-
-			tx.commit();
-		} catch (Exception e) {
-			tx.rollback();
-		} finally {
-			em.close();
-		}
-
-		emf.close();
 	}
-
 }
